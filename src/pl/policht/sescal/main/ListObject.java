450bd07fdf14763167967598b9fd741830300296
Michael Policht <michal85so@gmail.com>
2013-02-13 13:38:56 +0100

    dodalem klase obiektu, dopisalem obsluge pliku. problem: nie zapisuje do pliku obiektow(indexoutofboundexception)

---
+++ src/pl/policht/sescal/main/ListObject.java
@@ -0,0 +1,26 @@
+package pl.policht.sescal.main;
+
+public class ListObject {
+	private String name;
+	private String firEx;
+	private String secEx;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setFirEx(String firEx){
+		this.firEx = firEx;
+	}
+	public String getFirEx(){
+		return firEx;
+	}
+	public void setSecEx(String secEx){
+		this.secEx = secEx;
+	}
+	public String getSecEx(){
+		return secEx;
+	}
+}
